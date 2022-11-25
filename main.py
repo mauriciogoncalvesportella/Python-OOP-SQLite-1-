44d702aa3d12926760743d4047bea7ea1eb65b26
Maurício Gonçalves <116230925+Mauropython@users.noreply.github.com>
2022-11-25 01:52:25 +0000

    fizemos a consulta a banco, ficou top. mas houve algum erro no meio do caminho...

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-#Importar nosso arquivo Pessoa.py no diretório model
-from  model.Pessoa import Pessoa
+#Importa nosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
-mauricio = Pessoa(1, 'mauricio')
+mauricio = Pessoa(1, "Henrique Poyatos")
 print(mauricio)
 
-#Quero mostrar só o nome   
+#Quero mostrar só o nome
 print(mauricio.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+
+#CRiar um objeto com qualquer ator/atriz/diretor/diretora
+
+novo = Pessoa(0, 'Denzel Washington')
+
+#Olha que simples
+
+novo = pessoaDAO.save(novo)
+
+#Consulta o banco de novo..
+pessoas = pessoasDAO.getall(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+  
